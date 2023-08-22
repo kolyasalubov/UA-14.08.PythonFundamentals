2f24f82b81286716f7c2ec455e4a5b394f4871b6
Scania Ukraine / Kyrylo Romanenko <suaKIR@in.scania.ua>
2023-08-22 08:30:46 +0300

    Add line 29 between 28 and 42. Fix line 28 - delete excessive space

---
+++ KyryloRomanenko/HW1/PRACTICAL TASK1 [LESSON 1].py
@@ -0,0 +1,7 @@
+name = input('What is your name?:\n')
+age = input('How old are you?:\n')
+city = input('Where do you live?:\n')
+
+print(f'Hello, {name}')
+print(f'Your age is {age}')
+print(f'You live in {city}')
