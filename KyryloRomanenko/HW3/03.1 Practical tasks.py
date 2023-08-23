85dd69715963f411826ec4238f696940cd31d666
Scania Ukraine / Kyrylo Romanenko <suaKIR@in.scania.ua>
2023-08-23 18:03:19 +0300

    Add fist task to HW3

---
+++ KyryloRomanenko/HW3/03.1 Practical tasks.py
@@ -0,0 +1,22 @@
+# first task
+line_from_zen = input('Enter line from "Zen of Pyiton":\n').lower()
+
+count_better = 0
+count_never = 0
+count_is = 0
+
+for i in line_from_zen.split():
+    if i == 'better':
+        count_better += 1
+    elif i == 'never':
+        count_never += 1
+    elif i == 'is':
+        count_is += 1
+    else:
+        pass
+
+print(f'\nNumber "better" in a row: {count_better}')
+print(f'Number "never" in a row: {count_never}')
+print(f'Number "is" in a row: {count_is}\n')
+
+# second task
