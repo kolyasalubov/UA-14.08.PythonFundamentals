0e7a8a72f6cd031f138558237edd68302980eeb6
Ivanov Oleksii <oleksii.ivanov04@gmail.com>
2023-08-24 20:20:36 +0300

    added HW1

---
+++ IvanovOleksii/HW1/Task2.py
@@ -0,0 +1,12 @@
+a = int(input('Write first number: '))
+b = int(input('Write second number: '))
+
+print('A + B =', a + b)
+print('A - B =', a - b)
+print('A * B =', a * b)
+print('A / B =', a / b)
+print('A ** B =', a ** b)
+print('A // B =', a // b)
+print('A % B =', a % b)
+
+print()
