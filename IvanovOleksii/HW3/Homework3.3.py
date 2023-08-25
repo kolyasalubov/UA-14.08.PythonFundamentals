6d50ce962580ff5a3b7e7dbe16a1c5afed8d50f2
Ivanov Oleksii <oleksii.ivanov04@gmail.com>
2023-08-25 20:32:20 +0300

    add index in Homework3.3&commit it

--- IvanovOleksii/HW3/Homework3.3.py
+++ IvanovOleksii/HW3/Homework3.3.py
@@ -2,4 +2,4 @@ a = input('Enter first number: ')
 b = input('Enter second number: ')
 
 a, b = b, a
-print(f'Now first = {a} and second = {b}')
+print(f'Now first number = {a} and second number = {b}')
