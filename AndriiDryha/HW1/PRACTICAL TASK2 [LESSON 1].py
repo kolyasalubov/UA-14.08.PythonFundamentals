b26f90597c86ee23c10e18e80cd2da835f10c6ed
Dryha Andrii <4pa3.14rus@gmail.com>
2023-08-25 19:52:43 +0300

    added HW1

---
+++ AndriiDryha/HW1/PRACTICAL TASK2 [LESSON 1].py
@@ -0,0 +1,11 @@
+A = input("Enter number A \n")
+B = input("Enter number B \n")
+
+print('A + B = ', int(A)+int(B))
+print('A - B = ', int(A)-int(B))
+print('A * B = ', int(A)*int(B))
+print('A / B = ', int(A)/int(B))
+print('A ** B = ', int(A)**int(B))
+print('A // B = ', int(A)//int(B))
+print('A % B = ', int(A)%int(B))
+
