9235b91e989b86bc23f9c4e8915dec2318f02edc
Serhiy Didenko <didenkoserhiy123@gmail.com>
2023-08-25 16:56:11 +0300

    Added HW1 folder

---
+++ DidenkoSerhiy/HW1/first_task.py
@@ -0,0 +1,7 @@
+name = input("What is your name?\n")
+age = input("How old are you?\n")
+city = input("Where are do you live?\n")
+
+print("Hello,", name)
+print("Your age is", age)
+print("You live in the beautiful", city, "city")
---
+++ DidenkoSerhiy/HW1/second_task.py
@@ -0,0 +1,11 @@
+#first lesson, second task
+a = input("input the first number:")
+b = input("input the second number:")
+
+print('a+b =', int(a) + int(b))
+print('a-b =', int(a) - int(b))
+print('a*b =', int(a) * int(b))
+print('a/b =', int(a) / int(b))
+print('a**b =', int(a) ** int(b))
+print('a//b =', int(a) // int(b))
+print('a%b =', int(a) % int(b))
