56792f056aa5084f6da4e32da40bdde06f79d383
OleksandraKotovska <oleksandra12om@gmail.com>
2023-08-26 17:04:17 +0300

    added HW3

---
+++ OleksandraKotovska/HW3/1.py
@@ -0,0 +1,22 @@
+main_str = """
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+"""
+print(main_str.count('better'))
+print(main_str.count('never'))
+print(main_str.count('is'))
+print(main_str.upper())
+print(main_str.replace('i', '&'))
+
+
+
+
+
