f4153122f9a672625d41048e58c02166be34402e
Chernjachuk <chernjachuk8@gmail.com>
2023-08-27 14:32:35 +0300

    Task hw 4

--- CherniachukArtem/HW4/TAsk 1.py
+++ CherniachukArtem/HW4/TAsk 1.py
@@ -1,4 +1,5 @@
 #temperature conventer
+
 celsius_temp = float(input('Enter the temperature in celsius : '))
 if celsius_temp >= -273.15:
     print(celsius_temp,'C is equivalent', celsius_temp*9/5+32,'F')
