3ff2183a76fdc8dd50568fad8f2fa66982d26a05
Serhiy Didenko <didenkoserhiy123@gmail.com>
2023-08-27 22:12:09 +0300

    File deleted, didn't add a new HW branch

--- DidenkoSerhiy/homework_4.py
+++
@@ -1,8 +0,0 @@
-c = float(input("Input the temperature in Celcius: "))
-          
-f = c * 9/5 + 32
-
-if c > -273.1:
-    print(c,'\u00b0C' ' is equivalent', f,'\u00b0F')
-elif c <= -273.15:
-    print("Error: Temperature below absolute zero -273.15\u00b0С")
