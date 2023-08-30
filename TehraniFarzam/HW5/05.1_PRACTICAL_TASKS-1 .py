6d6babfd48bffae2128b13b3861c71d9b53fd162
Farzam Tehrani <farzamgt@gmail.com>
2023-08-30 11:16:25 +0300

    The suggested changes were modified in line 5

--- TehraniFarzam/HW5/05.1_PRACTICAL_TASKS-1 .py
+++ TehraniFarzam/HW5/05.1_PRACTICAL_TASKS-1 .py
@@ -2,7 +2,6 @@ list_of_integers = [1, 3, 4, 8, 14, 75, 99]
 list_of_floats = []
 
 for integer in list_of_integers:
-    float_value = float(integer)
-    list_of_floats.append(float_value)
+    list_of_floats.append(float(integer))
 
 print(list_of_floats)
