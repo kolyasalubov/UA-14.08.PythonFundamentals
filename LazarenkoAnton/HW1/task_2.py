bd844bfb310e57d7f1a825975c786ef333773f94
Anton Lazarenko <lazarenko.anton94@gmail.com>
2023-08-31 01:07:52 +0300

    added my lines to Zen.txt, added folders HW1, HW2 with tasks

---
+++ LazarenkoAnton/HW1/task_2.py
@@ -0,0 +1,12 @@
+
+x = int(input('Please write first number: '))
+y = int(input('Please write second number: '))
+
+print('There is your result:')
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** y)
+print(x // y)
+print(x % y)
