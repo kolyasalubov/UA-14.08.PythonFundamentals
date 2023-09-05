a7817457feee70387b1e51bd8c5f485b449b6d34
Scania Ukraine / Kyrylo Romanenko <suaKIR@in.scania.ua>
2023-09-05 08:14:16 +0300

    delete my test file

--- KyryloRomanenko/HW7/test.py
+++
@@ -1,9 +0,0 @@
-height = input("Enter height:")
-
-while True:
-    try:
-        height = float(height)
-        break
-    except ValueError:
-        print("Invalid input. Please enter a float.")
-        height = input("Enter height:")
