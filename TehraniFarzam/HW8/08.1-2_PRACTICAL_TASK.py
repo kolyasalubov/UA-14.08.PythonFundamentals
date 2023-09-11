9c68f929cc7fd652a44ff472c40eae1e98ce2de8
Farzam Tehrani <farzamgt@gmail.com>
2023-09-11 10:32:03 +0300

    comment added

--- TehraniFarzam/HW8/08.1-2_PRACTICAL_TASK.py
+++ TehraniFarzam/HW8/08.1-2_PRACTICAL_TASK.py
@@ -25,3 +25,18 @@ def is_valid_password(password):
         return False
     return True
 
+
+
+# password = input("Enter your password:\n")
+
+# if re.match("^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[$#@])(?=.{6,16}$).*", password):
+#     print("Nice pass! Psss, I won't tell anyone ;)")
+# else:
+#     print("Password is not valid. Check if you have:")
+# """
+# At least 1 letter between[a-z] and 1 letter between[A-Z].
+# At least 1 number between[0-9].
+# At least 1 character from [$#@].
+# Minimum length 6 characters.
+# Maximum length 16 characters.
+# """
