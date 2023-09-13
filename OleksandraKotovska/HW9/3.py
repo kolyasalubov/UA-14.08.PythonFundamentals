8e1d13af87aa789b62d9c7fc0294a0012ff7d59a
OleksandraKotovska <oleksandra12om@gmail.com>
2023-09-13 22:56:08 +0300

    added 3rd task

---
+++ OleksandraKotovska/HW9/3.py
@@ -0,0 +1,64 @@
+import tkinter as tk
+from tkinter import font
+from pyowm import OWM
+
+
+API_KEY = 'ef2206ff5da67de63306d0b143e20872'
+# ---------- FREE API KEY examples ---------------------
+
+owm = OWM(API_KEY)
+mgr = owm.weather_manager()
+
+def get_weather():
+    location = entry_field.get()
+    
+    observation = mgr.weather_at_place(location)
+    w = observation.weather
+    weather_info = f"Status: {w.detailed_status}\n"\
+                    f"Wind: {w.wind()}\n"\
+                    f"Humidity: {w.humidity}\n"\
+                    f"Temperature: {w.temperature('celsius')}\n"\
+                    f"Rain: {w.rain}\n"\
+                    f"Heat Index: {w.heat_index}\n"\
+                    f"Clouds: {w.clouds}"
+    label.config(text=weather_info)
+
+HEIGHT = 350
+WIDTH = 750
+
+
+root = tk.Tk()
+
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+root.title("Weather Application")
+canvas.pack()
+
+
+
+frame = tk.Frame(root, bg="deep sky blue", bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.92, relheight=0.1, anchor='n')
+
+entry_field = tk.Entry(frame, font=('Arial', 12))
+entry_field.place(relx=0, rely=0, relwidth=0.92, relheight=1)
+
+button = tk.Button(frame, 
+                   text="Get Weather", 
+                   bg="gray", fg="white", 
+                   font=('Arial', 8), 
+                   command=lambda: get_weather())
+button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
+
+
+
+lower_frame = tk.Frame(root, bg='gold', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.92, relheight=0.6, anchor='n')
+
+
+label = tk.Label(lower_frame, font=('Arial', 11))
+label.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+
+
+root.mainloop()
+
