9182914c193a5938745d8f4476ecf1b18dd1ba5d
Andrii Dryha <113519661+old-iron-skald@users.noreply.github.com>
2023-09-15 14:39:49 +0300

    Сorrected the changes

--- AndriiDryha/HW7/PRACTICAL TASKS [LESSON 7].py
+++ AndriiDryha/HW7/PRACTICAL TASKS [LESSON 7].py
@@ -14,43 +14,43 @@
 # print("The largest number of two entered numbers:", result)
 
 
-# Task 2
+# # Task 2
 
-Choice = input("Write the area of which figure you want to calculate: 'rectangle', 'triangle' or 'circle' \n")
+# Choice = input("Write the area of which figure you want to calculate: 'rectangle', 'triangle' or 'circle' \n")
 
-while True:
+# while True:
 
-    if Choice == "rectangle":
-        import Rectangle
-        print(Rectangle.rectangle_area)
-        break
-    if Choice == "triangle":
-        import Triangle
-        print(Triangle.triangle_area)
-        break
-    if Choice == "circle":
-        import Circle
-        print(Circle.circle_area)
-        break
+#     if Choice == "rectangle":
+#         import Rectangle
+#         print(Rectangle.rectangle_area)
+#         break
+#     if Choice == "triangle":
+#         import Triangle
+#         print(Triangle.triangle_area)
+#         break
+#     if Choice == "circle":
+#         import Circle
+#         print(Circle.circle_area)
+#         break
 
-# # Task 3
+# Task 3
 
-# def number_character(word):
+def number_character(word):
 
-#     symbols = list(set(list(word)))
-#     number = -1
-#     even_numbers = []
+    symbols = list(set(list(word)))
+    number = -1
+    even_numbers = []
 
-#     while True:
-#         if number < len(symbols):
-#             number = number + 1
-#             symbol_number = symbols[number] # number of the unique symbol in random order
-#             count = word.count(symbol_number) # count of the unique symbol in the entered string
-#             even_numbers.append(count)
-#         if len(even_numbers) == len(symbols):
-#             res = dict(map(lambda i,j : (i,j) , symbols,even_numbers))
-#             return res
+    while True:
+        if number < len(symbols):
+            number = number + 1
+            symbol_number = symbols[number] # number of the unique symbol in random order
+            count = word.count(symbol_number) # count of the unique symbol in the entered string
+            even_numbers.append(count)
+        if len(even_numbers) == len(symbols):
+            res = dict(map(lambda i,j : (i,j) , symbols,even_numbers))
+            return res
     
-# word = number_character(str(input("Enter everything \n")))
+word = number_character(str(input("Enter everything \n")))
 
-# print ("Count of the unique symbols in your input: " + str(word))
+print ("Count of the unique symbols in your input: " + str(word))
