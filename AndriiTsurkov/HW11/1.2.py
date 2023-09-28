a971e097131a62da1e538b8da8b7ea8d01cedb91
Andrii Tsurkov <daroga@ukr.net>
2023-09-28 16:53:42 +0300

    On branch AndriiTsurkov/HW11  Changes to be committed: 	modified:   AndriiTsurkov/HW11/1.2.py

--- AndriiTsurkov/HW11/1.2.py
+++ AndriiTsurkov/HW11/1.2.py
@@ -1,6 +1,8 @@
 class Day_of_week():
     def __init__(self, day: str) -> None:
+        self.days_of_week = {1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday", 7:"Sunday"}    
         self.main(self.check(day))
+
 
     def check(self, day: str) -> int:
         try:
@@ -11,22 +13,13 @@ class Day_of_week():
         return day
 
     def main(self, day: int) -> None:
-        match day:
-            case 1:
-                print(f"{day} is Monday.")
-            case 2:
-                print(f"{day} is Tuesday.")
-            case 3:
-                print(f"{day} is Wednesday.")
-            case 4:
-                print(f"{day} is Thursday.")
-            case 5:
-                print(f"{day} is Friday.")
-            case 6:
-                print(f"{day} is Saturday.")
-            case 7:
-                print(f"{day} is Sunday.")
-            case _:
-                print("Your number is not included in the days of the week.")
+
+        if day < 0 or day > 7:
+            print("Your number not included in the days of the week.")
+        else:
+            print(f"{day} is {self.days_of_week.get(day)}.")
+              
             
 Day_of_week(input("Plese enter number of day from week: "))
+
+
